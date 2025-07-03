a6a39af0b511a444cbb028b09ec28d7a89f99cc1
lukebryson <lukeb73@outlook.com>
2025-07-03 12:06:12 +0100

    Add custom colors and use new utilities

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,6 +6,13 @@ module.exports = {
       fontFamily: {
         gothic: ['Century Gothic', 'Arial', 'sans-serif'],
       },
+      colors: {
+        primary: '#1E3E89',
+        secondary: '#9375B2',
+        'secondary-dark': '#7a5e99',
+        accent: '#95C7ED',
+        'accent-dark': '#7bbbe2',
+      },
     },
   },
   plugins: [],
